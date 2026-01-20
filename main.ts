33cbd8ec89ea82f99dc54c6b9964f6a1f75f8e9e
Caleb-Campbell-1 <caleb.campbell@stu.ocsb.ca>
2026-01-20 15:48:02 -0500

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,46 @@
-/* Copyright (c) 2020 MTHS All rights reserved
- *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
-*/
-
-basic.showString('Hello, World!')
+// Copyright (c) 2026 MTHS All rights reserved
+// 
+// Created by: Caleb Campbell
+// 
+// Created on: Jan 2026
+// 
+// This program makes the <icrobit act like a flashlight
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 10; index++) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(100)
+    }
+})
+basic.showIcon(IconNames.Happy)
